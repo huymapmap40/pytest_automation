f74c424834aa211ae79159c68a106d9a7b527e02
huymapmap40 <huymapmap40@gmail.com>
2020-11-29 22:31:40 +0700

    remove unuse file

--- src/data/general/page_urls.py
+++ src/data/general/page_urls.py
@@ -3,6 +3,5 @@
 class PageUrls:
     GURU_99_PAGE = "http://demo.guru99.com"
     LOGIN_GURU_99_PAGE = "http://demo.guru99.com/v4"
-    GOBEAR_PAGE = "https://www.gobear.com/ph?x_session_type=UAT"
     TELECOM_PAGE = "http://demo.guru99.com/telecom/index.html"
     OPEN_WEATHER = "https://openweathermap.org/"
--- src/data/pages/deposit_page_constants.py
+++
@@ -1,8 +0,0 @@
-class DepositPageConstants:
-
-    TRANSACTION_ID = "Transaction ID"
-    ACCOUNT_ID = "Account No"
-    AMOUNT_CREDITED = "Amount Credited"
-    TRANSACTION_TYPE = "Type of Transaction"
-    DESCRIPTION = "Description"
-    CURRENT_BALANCE = "Current Balance"
